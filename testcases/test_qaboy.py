5f02759bdfcf1eba6c38abe040dbf0f2fecb3009
Vadim Gurkin <info@vgraph.ru>
2019-01-16 16:45:54 +0300

    Initial commit

---
+++ testcases/test_qaboy.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestQABoy(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_home_screen_components(self):
+        pass
+
+    def tearDown(self):
+        pass
